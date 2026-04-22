e5b5f204c22c1a561bf25515439b9df69ad63239
Fyelne <noel.andy53@gmail.com>
2026-04-22 09:28:52 +0200

    feat: wrap clean into functions

--- clean_data.r
+++ clean_data.r
@@ -3,95 +3,81 @@ library(dplyr)
 
 df <- read_csv('data/Data_Arbre_Input.csv', locale = locale(encoding = "UTF-8"))
 
-head(df, 5)
+clean_columns <- function(raw_data) {
+  
+  columns_to_drop <- c(
+    'OBJECTID', 'created_date', 'created_user',
+    'last_edited_user', 'last_edited_date',
+    'fk_prec_estim', 'CreationDate', 'Creator', 'EditDate', 'Editor',
+    'GlobalID', 'fk_nomtech', 'commentaire_environnement',
+    'src_geo'
+  ) # colonnes à drop (bdd, trop hétérogènes)
+  
+  # Affichage des arbres ayant les mêmes coordonées (probablement dupliqués)
+  print(sum(duplicated(df[c('X', 'Y')])))
+  
+  df_clean <- raw_data %>%
+    select(-all_of(columns_to_drop)) %>% # drop des colonnes non intéressantes
+    mutate(across(c(nomfrancais, nomlatin), ~na_if(., 'RAS'))) %>% # RAS = vide
+    mutate(fk_revetement = na_if(fk_revetement, ' ')) %>% # espace = vide
+    mutate(fk_stadedev = na_if(fk_stadedev, ' ')) %>%
+    mutate(fk_port = na_if(fk_port, ' ')) %>%
+    mutate(fk_pied = na_if(fk_pied, ' ')) %>%
+    mutate(
+      fk_port = trimws(fk_port),
+      fk_port = ifelse(fk_port == "Libre", "libre", fk_port)
+    ) %>% # retirer les espaces et les majs
+    mutate(dte_plantation = na_if(dte_plantation, '1970/01/01 00:00:02+00')) %>% # 1970/01/01 est une valeur par défaut donc on la set nulle
+    filter(!is.na(X) & !is.na(Y)) %>% # on supprime les valeurs sans géo pour les cartographies
+    filter(!(age_estim > 100 & fk_stadedev == "jeune")) %>% # (valeur abérante car décrit comme "jeune" mais centenaire)
+    distinct(X, Y, .keep_all = TRUE) # suppression des arbres ayant les mêmes coordonées
+  
+  return (df_clean)
+}
 
-columns_to_drop <- c(
-  'OBJECTID', 'created_date', 'created_user',
-  'last_edited_user', 'last_edited_date',
-  'fk_prec_estim', 'CreationDate', 'Creator', 'EditDate', 'Editor',
-  'GlobalID', 'fk_nomtech', 'commentaire_environnement',
-  'src_geo'
-) # colonnes à drop (bdd, trop hétérogènes)
-
-# Affichage des arbres ayant les mêmes coordonées (probablement dupliqués)
-print(sum(duplicated(df[c('X', 'Y')])))
-
-df_clean <- df %>%
-  select(-all_of(columns_to_drop)) %>% # drop des colonnes non intéressantes
-  mutate(across(c(nomfrancais, nomlatin), ~na_if(., 'RAS'))) %>% # RAS = vide
-  mutate(fk_revetement = na_if(fk_revetement, ' ')) %>% # espace = vide
-  mutate(fk_stadedev = na_if(fk_stadedev, ' ')) %>%
-  mutate(fk_port = na_if(fk_port, ' ')) %>%
-  mutate(fk_pied = na_if(fk_pied, ' ')) %>%
-  mutate(dte_plantation = na_if(dte_plantation, '1970/01/01 00:00:02+00')) %>% # 1970/01/01 est une valeur par défaut donc on la set nulle
-  filter(!is.na(X) & !is.na(Y)) %>% # on supprime les valeurs sans géo pour les cartographies
-  filter(!(age_estim > 100 & fk_stadedev == "jeune")) %>% # (valeur abérante car décrit comme "jeune" mais centenaire)
-  distinct(X, Y, .keep_all = TRUE) # suppression des arbres ayant les mêmes coordonées
-
-# Affichage du nouveau tableau clean
-head(df_clean)
-
-# Nombre de valeurs nulles par colonnes
-missing_counts <- df_clean %>%
-  summarise(across(everything(), ~sum(is.na(.)))) %>%
-  pivot_longer(everything(), names_to = "column", values_to = "missing_count") %>%
-  arrange(desc(missing_count))
-
-print(missing_counts)
-
-# convertir les string en factor
-df_clean <- df_clean %>%
-  mutate(
-    fk_arb_etat = factor(fk_arb_etat, levels = c(
-      "ABATTU", "EN PLACE", "Essouché", "Non essouché", "REMPLACE", "SUPPRIME"
-    )),
-    
-    fk_stadedev = factor(fk_stadedev, levels = c(
-      "adulte", "jeune", "senescent", "vieux"
-    )),
-    
-    #fk_port = factor(fk_port, levels = c(
-    #  "architecturé", "cépée", "Couronne", "couronné", "étêté", "libre",
-    #  "réduit", "réduit relâché", "rideau", "semi libre",
-    #  "têtard", "têtard relâché", "têtard de chat", "têtard de chat relaché"
-    #)),
-    
-    fk_pied = factor(fk_pied, levels = c(
-      "Bac de plantation", "Bande de terre", "fosse arbre", "gazon",
-      "Revetement non permeable", "terre", "toile tissée", "végétation"
-    )),
-    
-    fk_situation = factor(fk_situation, levels = c(
-      "Alignement", "Groupe", "Isolé"
-    )),
-    
-    feuillage = factor(feuillage, levels = c(
-      "Conifère", "Feuillu"
-    ))
-  )
-
-# convertir les string en date (toutes les valeurs sont en utc)
-df_clean <- df_clean %>%
-  mutate(
-    dte_plantation = as.POSIXct(dte_plantation, format = "%Y/%m/%d %H:%M:%S", tz = "UTC"),
-    dte_abattage   = as.POSIXct(dte_abattage, format = "%Y/%m/%d %H:%M:%S", tz = "UTC")
-  )
-
-# convertir les string en boolean
-df_clean <- df_clean %>%
-  mutate(
-    remarquable = case_when(
-      remarquable == "Oui" ~ TRUE,
-      remarquable == "Non" ~ FALSE,
-      TRUE ~ NA
-    ),
-    
-    fk_revetement = case_when(
-      fk_revetement == "Oui" ~ TRUE,
-      fk_revetement == "Non" ~ FALSE,
-      TRUE ~ NA
+setup_data <- function(raw_data) {
+  data <- raw_data
+  
+  # convertir les string en factor
+  data$fk_stadedev   <- factor(data$fk_stadedev,   levels = c("jeune", "adulte", "vieux", "senescent"))
+  data$fk_situation  <- factor(data$fk_situation)
+  data$fk_port       <- factor(data$fk_port)
+  data$fk_pied       <- factor(data$fk_pied)
+  data$fk_revetement <- factor(data$fk_revetement)
+  data$feuillage     <- factor(data$feuillage)
+  data$remarquable   <- factor(data$remarquable)
+  data$fk_arb_etat   <- factor(data$fk_arb_etat)
+  data$clc_quartier  <- factor(data$clc_quartier)
+  
+  # convertir les string en date (toutes les valeurs sont en utc)
+  data <- data %>%
+    mutate(
+      dte_plantation = as.POSIXct(dte_plantation, format = "%Y/%m/%d %H:%M:%S", tz = "UTC"),
+      dte_abattage   = as.POSIXct(dte_abattage, format = "%Y/%m/%d %H:%M:%S", tz = "UTC")
     )
-  )
+  
+  # convertir les string en boolean
+  data <- data %>%
+    mutate(
+      remarquable = case_when(
+        remarquable == "Oui" ~ TRUE,
+        remarquable == "Non" ~ FALSE,
+        TRUE ~ NA
+      ),
+      
+      fk_revetement = case_when(
+        fk_revetement == "Oui" ~ TRUE,
+        fk_revetement == "Non" ~ FALSE,
+        TRUE ~ NA
+      )
+    )
+  
+  return(data)
+}
+
+df_clean <- clean_columns(df)
+
+df_clean <- setup_data(df_clean)
 
 write_csv(df_clean, "data/Data_Arbre_Clean.csv")
 
